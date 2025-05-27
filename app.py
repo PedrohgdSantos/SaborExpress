c915efd1a6bfed9b1dc642ad6b1c0730c3ed9267
PHGDSantos <phgdsantoss@gmail.com>
2025-05-26 22:02:21 -0300

    Fechar  App

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+
+print(''' 
+▒█▀▀▀█ █▀▀█ █▀▀▄ █▀▀█ █▀▀█ 　 ▒█▀▀▀ █░█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ 
+░▀▀▀▄▄ █▄▄█ █▀▀▄ █░░█ █▄▄▀ 　 ▒█▀▀▀ ▄▀▄ █░░█ █▄▄▀ █▀▀ ▀▀█ ▀▀█ 
+▒█▄▄▄█ ▀░░▀ ▀▀▀░ ▀▀▀▀ ▀░▀▀ 　 ▒█▄▄▄ ▀░▀ █▀▀▀ ▀░▀▀ ▀▀▀ ▀▀▀ ▀▀▀ 
+      ''')
+
+print('1. Cadastrar  restaurante')
+print('2. Listar  restaurante')
+print('3. Ativar  restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(opcao_escolhida)
+
+def finalizar_app():
+    os.system('cls')
+    print('Encerrando o app\n')
+
+
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurante')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    finalizar_app()
